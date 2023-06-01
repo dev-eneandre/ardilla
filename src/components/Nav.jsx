095c207c7447f20b600f21c5d8a01befda4f77bc
unknown <iameneandre@gmail.com>
2023-06-01 11:08:32 -0700

    responsive navbar correction

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,40 +21,40 @@ const Nav = () => {
         className={`md:w-[470px] w-full  md:static absolute text-sm md:h-auto h-screen z-40 ${
           activeNav
             ? "md:translate-x-0  translate-x-[-150%] "
-            : "  bg-[rgba(36,0,83,1)] top-0 right-0 translate-x-[-0%]"
+            : "bg-[rgba(36,0,83,1)] top-0 right-0 translate-x-[-0%] "
         }`}
       >
         <GiHamburgerMenu
-          className="md:hidden block "
+          className="md:hidden block ml-10 mt-10 text-2xl"
           onClick={() => setactiveNav(!activeNav)}
         />
         <ul className="flex justify-between items-center md:flex-row flex-col">
-          <li className="flex items-center cursor-pointer">
+          <li className="flex items-center cursor-pointer md:py-0 py-5">
             Products <AiFillCaretDown />
           </li>
-          <li className="cursor-pointer">
+          <li className="cursor-pointer md:py-0 py-5">
             Business
             <span className="ml-2 px-1 text-sm rounded-md border-2 border-white">
               Beta
             </span>
           </li>
-          <li className="flex items-center cursor-pointer ">
+          <li className="flex items-center cursor-pointer md:py-0 py-5">
             Company <AiFillCaretDown />{" "}
           </li>
-          <li className="cursor-pointer">Learn</li>
+          <li className="cursor-pointer md:py-0 py-5">Learn</li>
           <div className="md:hidden block">
             <div
               className={`text-sm flex justify-between items-center ml-16 ${
-                activeNav ? "hidden" : "block"
+                activeNav ? "hidden" : "block flex-col  ml-0"
               } `}
             >
-              <button>Sign in</button>
-              <button className="bg-[rgb(136,7,247)]/100 py-3 px-8 rounded-3xl text-white ml-5 ">
+              <button className="md:py-0 py-5">Sign in</button>
+              <button className="bg-[rgb(136,7,247)]/100 py-3 px-8 rounded-3xl text-white ml-5  md:my-0 my-5">
                 Create Account
               </button>
-              <button className="bg-[rgba(214,171,252,1)] rounded-full p-2 ml-2">
+              <button className="bg-[rgba(214,171,252,1)] rounded-full p-2 ml-2 md:my-0 my-5">
                 <img src={World} alt="" />
-              </button>{" "}
+              </button>
               <FiChevronDown className="" />
             </div>
           </div>
